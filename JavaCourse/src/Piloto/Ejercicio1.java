43e37358b19eb79299b0aa2a3b51f4de15f0b023
David Antonio Olmos Ruiz <jamesdavid9810@outlook.com>
2024-01-29 14:48:19 -0600

    Primer commit
    Se empezo este proyecto con un pequeño mensaje

---
+++ JavaCourse/src/Piloto/Ejercicio1.java
@@ -0,0 +1,12 @@
+
+package Piloto;
+
+/**
+ *
+ * @author David Olmos
+ */
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo, este es mi primer codigo");
+    }
+}
